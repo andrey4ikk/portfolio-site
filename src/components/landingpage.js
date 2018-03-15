e6f348a027b1b531049f9553804f45ff506fb601
andk <andrey4ik.duka@gmail.com>
2018-03-16 00:59:02 +0200

    create

---
+++ src/components/landingpage.js
@@ -0,0 +1,44 @@
+import React , { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+class Landing extends Component {
+    render() {
+        return(
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                <Cell col={12}>
+                    <img 
+                        src="https://cdn3.savepice.ru/uploads/2018/3/16/5fa65c8b17bf0f658dfca9bdf769aebe-full.png"
+                        alt="avatar"
+                        className="avatar-img"
+                    />
+                    <div className="banner-text">
+                        <h1>Front End Developer</h1>
+                        <hr />
+                        <p>HTML/CSS/SASS/LESS | Bootstrap | Javascript | React | Redux</p>
+
+                        <div className="social-links">
+                            
+                            <a href="https://www.linkedin.com/in/andrey4ikk/" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-linkedin-square"  aria-hidden="true" />
+                            </a>
+
+                            <a href="https://github.com/andrey4ikk" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-github-square"  aria-hidden="true" />
+                            </a>
+
+                            <a href="https://www.facebook.com/andreyjkeee" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-facebook-square"  aria-hidden="true" />
+                            </a>
+                            
+
+                        </div>
+                    </div>
+                    </Cell>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default Landing;
